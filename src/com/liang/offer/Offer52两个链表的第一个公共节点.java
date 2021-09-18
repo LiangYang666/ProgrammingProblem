ad159006d6454a343ff9bc49ba57b5493837ff32
LiangYang666 <mail.liangyang@gmail.com>
2021-09-18 20:17:35 +0800

    两个链表的第一个公共节点

---
+++ src/com/liang/offer/Offer52两个链表的第一个公共节点.java
@@ -0,0 +1,38 @@
+package com.liang.offer;
+
+import com.liang.utils.ListNode;
+
+public class Offer52两个链表的第一个公共节点 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {   //方法一
+        int m=0, n=0;
+        ListNode p1=headA, p2=headB;
+        while(p1!=null){
+            p1 = p1.next;
+            m++;
+        }
+        while (p2!=null){
+            p2 = p2.next;
+            n++;
+        }
+        System.out.println(m);
+        System.out.println(n);
+        int err = m-n;
+        System.out.println(err);
+        while (err>0) {
+            headA = headA.next;
+            err--;
+        }
+        while (err<0){
+            headB = headB.next;
+            err++;
+        }
+        while (headA!=null){
+            if(headA==headB){
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
