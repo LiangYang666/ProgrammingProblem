54e9da60a0af2425eb04044ca4c9b1b5557da08f
LiangYang666 <mail.liangyang@gmail.com>
2021-10-11 12:19:32 +0800

    位运算 有难度

---
+++ src/com/liang/offer/Offer65不用加减乘除做加法.java
@@ -0,0 +1,18 @@
+package com.liang.offer;
+
+public class Offer65不用加减乘除做加法 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = -20;
+        System.out.println(add(100, -90));
+    }
+    static
+    public int add(int a, int b) {
+        while(b!=0){
+            int c = (a&b)<<1;
+            a^=b;
+            b = c;
+        }
+        return a;
+    }
+}
