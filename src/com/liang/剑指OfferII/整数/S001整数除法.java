70e5d02b1e28e0f47101ce0ee8ac2227e0979dfb
LiangYang666 <mail.liangyang@gmail.com>
2021-12-16 10:08:44 +0800

    再现整数除法

--- src/com/liang/剑指OfferII/整数/S001整数除法.java
+++ src/com/liang/剑指OfferII/整数/S001整数除法.java
@@ -4,38 +4,33 @@ public class S001整数除法 {
     public static void main(String[] args) {
         System.out.println(divide(-2147483648, 2));
     }
-
     static
-    public int divide(int a, int b) {
-        int res = 0;
-        // 特殊情况讨论
-        if (a==Integer.MIN_VALUE) {
-            if (b==-1)  return Integer.MAX_VALUE;
-            else if(b==1)   return Integer.MIN_VALUE;
-            else if(b==Integer.MIN_VALUE)   return 1;
-            else{
+    public int divide(int a, int b){
+        int r=0;
+        boolean sign = a > 0 == b > 0;
+        if(b==1)    return a;
+        if(a==Integer.MIN_VALUE){
+            if (b==Integer.MIN_VALUE)   return 1;
+            else if (b==-1) return Integer.MAX_VALUE;
+            else {
                 a+=Math.abs(b);
-                res +=1;
+                r+=1;
             }
         }
-        System.out.println(a);
-        if(b==Integer.MIN_VALUE) return 0;
-        if(a==0)    return 0;
-        boolean sign = (a < 0) == (b < 0);
-        //运算
+        if (b==Integer.MIN_VALUE)   return 0;
+        if (a==0)   return 0;
         a = Math.abs(a);
         b = Math.abs(b);
-        String bStr = Integer.toBinaryString(b);
-        int index = bStr.indexOf("1");
-        int bitNow = 31-(bStr.length()-index);
-        System.out.println(bitNow);
-        while(bitNow>=0){
-            if (b<<bitNow <a){
-                a -= b<<bitNow;
-                res += 1<<bitNow;
+        int bitNow = Integer.toBinaryString(a).length()-Integer.toBinaryString(b).length();
+        while (bitNow>=0){
+            if(a>= (b<<bitNow)){
+                a-=b<<bitNow;
+                r+=1<<bitNow;
             }
             bitNow--;
+
         }
-        return sign?res:-res;
+        return sign?r:-r;
     }
+
 }
