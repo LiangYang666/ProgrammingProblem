a62e42bf7308ebfc766826cf1ca4b48acb433d45
LiangYang666 <mail.liangyang@gmail.com>
2022-02-22 10:19:12 +0800

    Hash 集合

---
+++ src/com/liang/面试知识点/集合/HashMapDemo.java
@@ -0,0 +1,10 @@
+package com.liang.面试知识点.集合;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        System.out.println(a=b);
+    }
+
+}
