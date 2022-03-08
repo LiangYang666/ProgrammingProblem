529a74f8c65c61fb0a4045a2e04a6aeb8e6d0aef
LiangYang666 <mail.liangyang@gmail.com>
2022-03-08 23:16:00 +0800

    牛客4星阿里

---
+++ src/com/liang/笔试/牛客阿里4星/子集.java
@@ -0,0 +1,67 @@
+package com.liang.笔试.牛客阿里4星;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class 子集 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        int[] rs = new int[T];
+        for (int i = 0; i < T; i++) {
+            int n = in.nextInt();
+            int[] xs = new int[n];
+            int[] ys = new int[n];
+            for (int j = 0; j < n; j++) {
+                xs[j] = in.nextInt();
+            }
+            for (int j = 0; j < n; j++) {
+                ys[j] = in.nextInt();
+            }
+            rs[i] = handleEach(xs, ys);
+            System.out.println(rs[i]);
+        }
+//        for (int i = 0; i < T; i++) {
+//        }
+
+    }
+    public static int handleEach(int[] xs, int[] ys){
+        int n = xs.length;
+        int[][] all = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            all[i][0] = xs[i];
+            all[i][1] = ys[i];
+        }
+        Arrays.sort(all, (o1, o2) -> {
+            if (o1[0]-o2[0]==0){
+                return o1[1]-o2[1];
+            } else {
+                return o1[0]-o2[0];
+            }
+        });
+        int[][] low = new int[n][];
+        low[0] = all[0];
+        int last = 0;
+        for (int[] temp : all) {
+            if (temp[0]<low[last][0] && temp[1]<low[last][1]){
+                low[++last] = temp;
+            } else{
+
+            }
+        }
+//        System.out.println(Arrays.deepToString(all));
+        return 0;
+    }
+    int getFirstMin(int[][] low, int last, int[] temp){
+        int l=0;
+        int r=last;
+        while(l<r){
+//            if()
+
+        }
+        return 0;
+
+    }
+}
---
+++ src/com/liang/笔试/牛客阿里4星/小强爱数学.java
@@ -0,0 +1,36 @@
+package com.liang.笔试.牛客阿里4星;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 小强爱数学 {
+    public static void main(String[] args) {
+        System.out.println((-10+4*10)%4);
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] rs = new int[T];
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+//            System.out.println(a+" "+b+" "+n);
+            rs[i] = handle(a,b,n);
+//            System.out.println(rs[i]);
+        }
+        for (int i = 0; i < T; i++) {
+            System.out.println(rs[i]);
+        }
+    }
+    public static int handle(long a, long b, int n){
+        long mod = 1000000007;
+        long[] rs = new long[n + 1];
+        rs[0] = 2;
+        if(n==0)    return 2;
+        rs[1] = a%mod;
+        for (int i = 2; i < n+1; i++) {
+            rs[i] = (a*rs[i-1]%mod-b*rs[i-2]%mod+mod)%mod;
+        }
+//        System.out.println(Arrays.toString(rs));
+        return (int)rs[n];
+    }
+}
