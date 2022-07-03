c54fa94724bd923781dda315a7d00a772d30f0c3
LiangYang <mail.liangyang@gmail.com>
2022-07-03 21:48:41 +0800

    -

--- src/com/liang/面试知识点/集合/HashMapDemo.java
+++ src/com/liang/面试知识点/集合/HashMapDemo.java
@@ -1,10 +1,20 @@
 package com.liang.面试知识点.集合;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class HashMapDemo {
     public static void main(String[] args) {
         Integer a = 1;
         Integer b = 2;
         System.out.println(a=b);
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(1,2);
+        map.size();
+        ConcurrentHashMap<Object, Object> cMap = new ConcurrentHashMap<>();
+        cMap.put(2,1);
+        cMap.put(2,2);
+        cMap.size();
     }
 
 }
