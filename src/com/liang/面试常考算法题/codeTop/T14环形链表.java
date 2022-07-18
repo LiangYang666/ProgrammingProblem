94bd595db5ab712eca149a27d81825d7c7d0a578
yangliang.20 <yangliang.20@bytedance.com>
2022-07-18 22:05:04 +0800

    -

--- src/com/liang/面试常考算法题/codeTop/T14环形链表.java
+++ src/com/liang/面试常考算法题/codeTop/T14环形链表.java
@@ -5,12 +5,10 @@ import com.liang.utils.ListNode;
 
 public class T14环形链表 {
     public boolean hasCycle(ListNode head) {
-        if (head==null || head.next==null){
-            return false;
-        }
-        ListNode fast = head;
-        ListNode slow = head.next;
-        while (fast!=slow){
+        if (head==null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow!=fast){
             if (fast==null || fast.next==null){
                 return false;
             }
