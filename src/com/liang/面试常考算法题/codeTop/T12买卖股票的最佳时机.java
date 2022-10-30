91dde5eb018c0ce80639ae56bb9d7579f2ea1104
yl_SurfacePro8 <mail.liangyang@gmail.com>
2022-10-30 09:05:31 +0800

    -

--- src/com/liang/面试常考算法题/codeTop/T12买卖股票的最佳时机.java
+++ src/com/liang/面试常考算法题/codeTop/T12买卖股票的最佳时机.java
@@ -3,13 +3,14 @@ package com.liang.面试常考算法题.codeTop;
 import java.util.LinkedList;
 
 public class T12买卖股票的最佳时机 {
-    public int maxProfit(int[] prices) {    // 一次遍历
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+    public int maxProfit(int[] prices) {
+        int[] dp = new int[2];
+        dp[0] = Integer.MIN_VALUE;
+        dp[1] = Integer.MIN_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            maxProfit = Math.max(maxProfit, prices[i]-minPrice);
-            minPrice = Math.min(prices[i], minPrice);
+            dp[0] = Math.max(dp[0], -prices[i]);
+            dp[1] = Math.max(dp[1], dp[0]+prices[i]);
         }
-        return maxProfit;
+        return dp[1];
     }
 }
